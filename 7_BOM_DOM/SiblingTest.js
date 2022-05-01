e836fe47c9cd00cd717394625e047e2ff551a710
KukjinKim0211 <rnrwls0211@naver.com>
2022-05-02 03:12:38 +0900

    sibling test
    sibling test.
    node를 검색할때 공백도 들어가는것 유의

---
+++ 7_BOM_DOM/SiblingTest.js
@@ -0,0 +1,14 @@
+// Sibling : 같은 Parent를 가진 노드의 관계
+/*
+  parentNode : 부모 노드 반환
+  childNodes : 요소의 자식 노드를 반환. 반환형은 NodeList
+  firstChild : 자식 노드 중 첫 번째 자식을 반환. 자식이 없을 경우 Null을 반환
+  lastChild  : 자식 노드 중 두 번째 자식을 반환. 자식이 없을 경우 Null을 반환
+  nextSibling : 부모의 childNodes 목록 중 자신 다음에 있는 노드를 반환. 없을 경우 null을 반환
+  previousSibling : 부모의 childNodes 목록 중 자신 이전에 있는 노드를 반환. 없을 경우 null을 반환
+*/
+
+const div = document.querySelector("#div3");
+const divChild = div.children[0];
+console.log(div);
+console.log(divChild.nextSibling.nextSibling);
